fd2c74e8de7b83d5d8a9ebe5a904077e6b8baff6
Leonardo Ferreira <ferreira_master@hotmail.com>
2021-08-02 09:55:23 -0300

    new schema on the layout structure

--- components/prediction.py
+++ components/prediction.py
@@ -24,7 +24,7 @@ project_list=load_input_dict().keys()
 
 prediction_layout=html.Div(
         [
-            html.H1("IBM Customers Churn Prediction", className="text-center bottom64"), 
+            html.H1("IBM Customers Churn Prediction", className="text-center bottom32 top16"), 
             html.Div(id="customer-input-list", className="bottom32"),
             html.Div([html.Button(["Predict"], id="btn-predict", className="buttonStyle bottom16"),
                       dbc.Alert(
@@ -35,7 +35,7 @@ prediction_layout=html.Div(
                             className="alert-card-style"
                         )], 
                     className="text-center bottom16"),
-            dcc.Loading(html.Div(id="prediction-result"))
+            html.Div(id="prediction-result", style={"min-height":"250px"})
         ])
 
 
@@ -53,8 +53,12 @@ def generating_dropdowns(dropdown_data):
                 [
                     dbc.Col([
                         html.Div([
-                            html.H3(["Demographic Features", html.Span(html.I(className="fas fa-question-circle font-sm"), 
+                            html.H3(["Demographic Features", html.Span(html.I(className="fas fa-question-circle font-sm", id="tooltip-demographic"), 
                                             style={"marginLeft":"5px"})], className="bottom16"),
+                            dbc.Tooltip(
+                                "Explanation about Demographic features.",
+                                target="tooltip-demographic",
+                            ),
                             dbc.Row([
                                 dbc.Col(html.Div(
                                     [
@@ -65,46 +69,51 @@ def generating_dropdowns(dropdown_data):
                                     ]), lg=3, className="bottom16"),
                                 dbc.Col(html.Div(
                                     [
-                                        html.H4("Senior Citizen", className="font-sm"),
-                                        dcc.Dropdown(id="input-Senior Citizen", 
-                                                    options=[{"label":val, "value":val} \
-                                                        for val in dropdown_data["Senior Citizen"]], 
+                                        html.H4("Partner", className="font-sm"),
+                                        dcc.Dropdown(id="input-Partner", 
+                                                options=[{"label":val, "value":val} \
+                                                    for val in dropdown_data["Partner"]],
                                                     value="No")
                                     ]), lg=3, className="bottom16"),
                                 dbc.Col(html.Div(
                                     [
-                                        html.H4("Partner", className="font-sm"),
-                                        dcc.Dropdown(id="input-Partner", 
-                                                options=[{"label":val, "value":val} \
-                                                    for val in dropdown_data["Partner"]])
-                                    ]), lg=3, className="bottom16"),
-                                dbc.Col(html.Div(
-                                    [
-                                        html.H4("Dependents", className="font-sm"),
+                                        html.H4(["Dependents", html.Span("*", style={"color":"red"})], className="font-sm"),
                                         dcc.Dropdown(id="input-Dependents", 
                                                 options=[{"label":val, "value":val} \
                                                     for val in dropdown_data["Dependents"]],
+                                                    value=None)
+                                    ]), lg=3, className="bottom16"),
+                                dbc.Col(html.Div(
+                                    [
+                                        html.H4("Senior Citizen", className="font-sm"),
+                                        dcc.Dropdown(id="input-Senior Citizen", 
+                                                    options=[{"label":val, "value":val} \
+                                                        for val in dropdown_data["Senior Citizen"]], 
                                                     value="No")
                                     ]), lg=3, className="bottom16")])
                             ], className="style-input-cards")
                     ], lg=6),
                     dbc.Col([
                         html.Div([
-                            html.H3(["Contract settings", html.Span(html.I(className="fas fa-question-circle font-sm"), 
+                            html.H3(["Contract settings", html.Span(html.I(className="fas fa-question-circle font-sm", id="tooltip-contract"), 
                                             style={"marginLeft":"5px"})], className="bottom16"),
+                            dbc.Tooltip(
+                                "Contract Features & billing options.",
+                                target="tooltip-contract",
+                            ),
                             html.Div(dbc.Row(
                                 [
                                     dbc.Col(html.Div(
                                         [
-                                            html.H4("Contract", className="font-sm"),
+                                            html.H4(["Contract", html.Span("*", style={"color":"red"})], className="font-sm"),
                                             dcc.Dropdown(id="input-Contract", 
                                                     options=[{"label": "Monthly" if val == "Month-to-month" else val, "value":val} \
                                                         for val in dropdown_data["Contract"]],
-                                                        value="Month-to-month")
+                                                        value=None)
                                         ]), lg=4, className="bottom16"),
                                     dbc.Col(html.Div(
                                         [
-                                            html.H4("Billing option", className="font-sm"),
+                                            html.H4("Billing Option", className="font-sm"),
                                             dcc.Dropdown(id="input-Paperless Billing", 
                                                     options=[{"label":"Paperless" if val == "Yes" else "Paper", "value":val} \
                                                         for val in dropdown_data["Paperless Billing"]],
@@ -132,8 +141,12 @@ def generating_dropdowns(dropdown_data):
                 [
                     dbc.Col([
                         html.Div([
-                            html.H3(["Service Features", html.Span(html.I(className="fas fa-question-circle font-sm"), 
+                            html.H3(["Service Features", html.Span(html.I(className="fas fa-question-circle font-sm", id="tooltip-services"), 
                                             style={"marginLeft":"5px"})], className="bottom16"),
+                            dbc.Tooltip(
+                                "Explanation about the services.",
+                                target="tooltip-services",
+                            ),
                             html.Div([dbc.Row(
                                 [
                                     dbc.Col(html.Div(
@@ -157,7 +170,7 @@ def generating_dropdowns(dropdown_data):
                                             html.Div([
                                                 html.Span(html.I(className="fas fa-plus-circle"), 
                                                             style={"margin-right":"5px", "color":"#9c7d80"}),
-                                                html.Span("More Services", className="font-sm")
+                                                html.Span("Extra Services", className="font-sm")
                                                 ], style={"cursor":"pointer", "paddingTop":"6px"}, id="load-more-button")
 
                                         ], style={"display":"flex", 
@@ -229,8 +242,12 @@ def generating_dropdowns(dropdown_data):
                     ], lg=6),
                     dbc.Col([
                         html.Div([
-                            html.H3(["Tenure & Charges", html.Span(html.I(className="fas fa-question-circle font-sm"), 
+                            html.H3(["Tenure & Charges", html.Span(html.I(className="fas fa-question-circle font-sm", id="tooltip-numericals"), 
                                             style={"marginLeft":"5px"})], className="bottom16"),
+                            dbc.Tooltip(
+                                "Tenure in months and Billing Charges.",
+                                target="tooltip-numericals",
+                            ),
                             dbc.Row(
                                 [
                                     dbc.Col(html.Div(
@@ -261,7 +278,7 @@ def generating_dropdowns(dropdown_data):
                                                     )
                                         ]), lg=3, className="bottom16")
                                 ])
-                        ], className="style-input-cards")
+                        ], className="style-input-cards", style={"maxHeight":"156px"})
                     ], lg=6)
                 ]
             )),
@@ -283,8 +300,6 @@ def toggle_left(n, btn_pred, is_open):
         raise PreventUpdate
     else:
         button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    print(button_id)
 
     if button_id == "load-more-button":
         return not is_open
@@ -325,13 +340,15 @@ def getting_input_parameters(btn_clicks, gender, senior_citzen, partner, \
         return dash.no_update, True
 
     else: 
+
         time.sleep(2)
+
         user_new_input = create_input_table(all_inputs)
 
         predictions=prediction(user_new_input)
 
         will_churn="The model prediction is that this customer has a high chance to Churn this time."
         will_no_churn="The model prediction is that this customer has a low chance to Churn and will not Churn this time."
-        
+
         return  dbc.Alert(f"{will_churn if int(predictions['Label'][0]) == 1 else will_no_churn}", className="alert-card-style"), False
   
